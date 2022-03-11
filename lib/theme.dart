709c067bc4bfeeb9a982dbcdc0097bbee2499baf
ninjaasmoke <nithins674@gmail.com>
2022-03-11 21:59:25 +0530

    basic setup

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  backgroundColor: Colors.black,
+  scaffoldBackgroundColor: Colors.black,
+  fontFamily: 'Product',
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color(0xff141418),
+  ),
+);
